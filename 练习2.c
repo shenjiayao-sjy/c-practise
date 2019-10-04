91cf52ded5e167fe75978cc7c3c0dfaa9d60d3cf
shenjiayao.sjy <1107570617@qq.com>
2019-10-04 21:30:18 +0800

    update 2.c

--- 练习2.c
+++ 练习2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(void)
+{       
+        int num;
+                printf("请输入一个四位整数:");
+                scanf("%d", &num);
+                
+                printf("它的个位是%d,十位是%d,百位是%d,千位是%d。 \n ",num%10,(num/10)%10,(num/100)%10,(num/1000)%10);
+                
+                return 0;
+}               
